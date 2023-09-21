4badb12fa10279351aa86b3642228ae1e151bb61
creed-wang <creed.wang@kakaocorp.com>
2023-09-21 16:46:42 +0900

    👔 use custom prompt

--- kakaosync/kakaosync/kakaosync_chatbot.py
+++ kakaosync/kakaosync/kakaosync_chatbot.py
@@ -2,9 +2,30 @@ import asyncio
 
 from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.conversational_retrieval.prompts import \
+    CONDENSE_QUESTION_PROMPT
+from langchain.chains.llm import LLMChain
+from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
 from langchain.vectorstores import VectorStore
+
+CUSTOM_QA_PROMPT_TEMPLATE = """아래 Context를 활용해서 Question에 대한 답변을 해줘.
+카카오소셜, 카카오싱크, 카카오톡채널에 관련된 대답만 하고, 그 외에는 답변할 수 없다고 대답해.
+
+<context>
+{context}
+</context>
+
+<Question>
+{question}
+</Question>
+Answer:
+"""
+CUSTOM_QA_PROMPT = PromptTemplate(
+    template=CUSTOM_QA_PROMPT_TEMPLATE, input_variables=["context", "question"]
+)
 
 
 class KakaoSyncChatBot:
@@ -27,16 +48,27 @@ class KakaoSyncChatBot:
     async def ask_question(self, question):
         callback_handler = AsyncIteratorCallbackHandler()
         question_llm = ChatOpenAI(temperature=0, verbose=True)
+        question_generator = LLMChain(
+            llm=question_llm,
+            prompt=CONDENSE_QUESTION_PROMPT,
+            verbose=True
+        )
         streaming_llm = ChatOpenAI(
             model="gpt-3.5-turbo",
             streaming=True,
             callbacks=[callback_handler],
             temperature=0
         )
-        self.conversation = ConversationalRetrievalChain.from_llm(
-            llm=streaming_llm,
+        doc_chain = load_qa_chain(
+            streaming_llm,
+            chain_type="stuff",
+            prompt=CUSTOM_QA_PROMPT,
+            verbose=True
+        )
+        self.conversation = ConversationalRetrievalChain(
+            combine_docs_chain=doc_chain,
             retriever=self.db.as_retriever(),
-            condense_question_llm=question_llm,
+            question_generator=question_generator,
             memory=self.memory,
             verbose=True
         )
